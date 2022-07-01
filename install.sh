21b7eb4714097d7da608ff340376b32a134d3c9f
WH173 5P1D3R <wh1735p1d3r@gmail.com>
2022-07-01 22:19:19 +0530

    ```tmx-pkgs```

---
+++ install.sh
@@ -0,0 +1,89 @@
+#!/data/data/com.termux/files/usr/bash
+
+echo -e '\e[1;92m
+
+    
+
+████████╗███╗░░░███╗██╗░░██╗  ██████╗░██╗░░██╗░██████╗░░██████╗
+╚══██╔══╝████╗░████║╚██╗██╔╝  ██╔══██╗██║░██╔╝██╔════╝░██╔════╝
+░░░██║░░░██╔████╔██║░╚███╔╝░  ██████╔╝█████═╝░██║░░██╗░╚█████╗░
+░░░██║░░░██║╚██╔╝██║░██╔██╗░  ██╔═══╝░██╔═██╗░██║░░╚██╗░╚═══██╗
+░░░██║░░░██║░╚═╝░██║██╔╝╚██╗  ██║░░░░░██║░╚██╗╚██████╔╝██████╔╝
+░░░╚═╝░░░╚═╝░░░░░╚═╝╚═╝░░╚═╝  ╚═╝░░░░░╚═╝░░╚═╝░╚═════╝░╚═════╝░
+\e[0m'
+printf "\n"
+printf "               \e[101m\e[1;77m  >>  Script By WH173 5P1D3R << \e[0m\n"
+printf "\n"
+printf " \e[1;93m <<----[THANKS FOR USING THIS SCRIPT]---->>\e[0m \n"
+
+echo -e "\e[34mSTABLE CONNECTION [\e[92m✓\e[34m]\e[0m"
+echo -e "\e[34mCHECKING ENVIRONMENT [\e[92m✓\e[34m]\e[0m"
+dpkg --configure -a 
+echo -e "\e[34mPACKAGES BEING INSTALLED WAIT....\e[0m"
+pkg update -y
+pkg upgrade -y
+apt-get update -y
+apt-get uprade -y
+pkg install python -y
+pip install lolcat -y
+pip install wheel -y
+pkg install python2 -y 
+pkg install php
+pkg install openssh 
+pkg install curl 
+pkg install python 
+pkg install nano 
+pkg install toilet
+pkg install cowsay -y
+pkg install perl 
+pkg install nmap 
+pkg install clang 
+pkg install bash 
+pkg install w3m 
+pkg install hydra
+pkg install figlet  
+pkg install tar 
+pkg install unzip 
+pkg install tor 
+pkg install wget 
+pkg install wcalc
+pkg install bmon
+pkg install proot
+pkg install toilet 
+pkg install net-tools 
+pkg install macchanger
+pkg install ruby 
+pkg install fish 
+pkg install help
+pip2 install --upgrade pip
+pip2 install requests
+pip2 install Mechanize
+pip2 install termcolor
+gem install lolcat
+pkg install libffi-dev -y 
+pkg install libgmp-dev -y 
+pkg insall libpcap-dev -y 
+pkg install postgresql-dev -y
+pkg install readline-dev -y 
+pkg install libsqlite-dev -y 
+pkg install openssl-dev -y 
+pkg install libxml2-dev -y 
+pkg install libxslt-dev -y 
+pkg install ncurses-dev -y 
+pkg install ruby-dev -y 
+pkg install libgrpc-dev -y
+apt-get install nodejs
+apt-get install libwebp
+apt-get install mc
+apt-get install ffmpeg
+apt-get install wget
+apt-get install tesseract
+apt-get install nmap
+pkg install nano
+apt autoupdate
+
+echo
+echo
+echo
+echo
+echo "All dependencies have been installed, Thank You For Using This Script"
